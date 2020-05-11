481a22d9b65418b8896fb5d4bf796adb3fa2e6bd
jnr25 <powiuke@gmail.com>
2020-05-11 18:04:46 +0300

    added mars api

--- version/app.js
+++ version/app.js
@@ -11,6 +11,14 @@ app.get('/', function (req, res) {
     res.send(response);
 
 });
+app.get('/mars', function (req, res) {
+
+  response = 'This is version MARS of the app.' + '\n';
+
+  //send the response to the client
+  res.send(response);
+
+});
 
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
